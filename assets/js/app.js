273e736926e3fc966bcd4ecb1e76b9cc852f6194
上野佑樹 <>
2019-10-15 23:50:34 +0800

    課題
    １０１５課題です

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+$(function(){
+  $("#js-show-btn").on("click", function() {
+    $("#js-target-element").fadeIn(11);
+  });
+
+   $("#js-hide-btn").on("click", function() {
+     $("#js-target-element").fadeOut(11);
+   });
+
+
+ });
